4096fd6cd939062d7362817361df4716498f0249
13732283659 <43004080+13732283659@users.noreply.github.com>
2024-09-05 14:20:05 +0800

    Update main.cpp

--- demo01/main.cpp
+++ demo01/main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
-#include <stdio.h> 
+
+
 int main(int argc,char *argv[])
 {
+    std::cout << "****out test****" << std::endl;
     std::cout << "hello world" << std::endl;
     return 0;
 }
